37c44b412c87c99840998ce9cda0a78ddccfe193
Utkarsh Yadav <yadavutkarsh207@gmail.com>
2020-05-21 16:49:25 +0530

    first Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# What is this?
+
+Help you getting perfect shadow for the non-designer and adds to beautiful image padding .
+
+# Installation
+
+`npm i frameit --save`
+
+Using in your code
+
+```bash
+import {frameit} from frameit
+
+frameit({
+    shadow_type: 'soft',
+    padding: false
+})
+```
+
+## Using Options
+
+frameit has 2 options for now.. See the example below:
+
+* *shadow_type* - _hard / soft_ (Defaults as soft)
+* *padding* - _boolean (Defaults as false)
---
+++ index.js
@@ -0,0 +1,18 @@
+function frameit(options) {
+    let images = document.querySelectorAll('.frameit');
+
+    if(options.shadow_type === 'hard')
+        options.shadow_type = '0px'
+    else
+        options.shadow_type = '15px'
+
+    images.forEach(image =>{
+        image.getElementsByClassName.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0,12)`;
+
+        if(options.padding) {
+            image.getElementsByClassName.padding = '1em';
+        }
+    })
+}
+
+module.exports.frameit = frameit;
